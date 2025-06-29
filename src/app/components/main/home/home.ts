e8145609c9ca323f5b793152fc4d4cacb962f9c3
Vladimir Gulev <vladinson009@icloud.com>
2025-06-29 23:10:33 +0200

    renamed components

--- src/app/components/main/home/home.ts
+++ src/app/components/main/home/home.ts
@@ -1,38 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { trigger, style, animate, transition } from '@angular/animations';
-export interface TestimonialType {
+import carouselAnimation from '../../../animations/carouselAnimation';
+import slideAnimation from '../../../animations/slideAnimation';
+export interface HomeType {
   name: string;
   message: string;
   imageUrl?: string;
   role?: string;
 }
 @Component({
-  selector: 'app-testimonial',
+  selector: 'app-home',
   imports: [MatButtonModule, CommonModule],
-  templateUrl: './testimonial.html',
-  styleUrl: './testimonial.css',
-  animations: [
-    trigger('slideAnimation', [
-      transition(':increment', [
-        style({ transform: 'translateX(100%)', opacity: 0 }),
-        animate(
-          '300ms ease-out',
-          style({ transform: 'translateX(0)', opacity: 1 })
-        ),
-      ]),
-      transition(':decrement', [
-        style({ transform: 'translateX(-100%)', opacity: 0 }),
-        animate(
-          '300ms ease-out',
-          style({ transform: 'translateX(0)', opacity: 1 })
-        ),
-      ]),
-    ]),
-  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+  animations: [carouselAnimation(800), slideAnimation(800, 'Y')],
 })
-export class Testimonial implements OnInit, OnDestroy {
+export class Home implements OnInit, OnDestroy {
   private intervalId: null | number = null;
   public activeIndex = 0;
 
@@ -46,21 +30,20 @@ export class Testimonial implements OnInit, OnDestroy {
       clearInterval(this.intervalId);
     }
   }
-  public testimonials: TestimonialType[] = [
+  public testimonials: HomeType[] = [
     {
-      name: 'Jane Doe',
-      message:
-        ' This pet shop cha c ch lent service! This pet shop changed my life. Excellellent service! This pet shop changed my life. Excellellent service! This pet shop changed my life. Excellellent service! This pet shop changed my life. Excellellent service! This pet sho life. Excellellent service! This pet sho life. Excellellent service! This pet sho life. Excellellent service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This petnt service! This pet sho life. Excellellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service! This pet shop changed my life. Excellent service!',
+      name: 'Luka',
+      message: 'This is an amazing dog',
       imageUrl:
         'https://images.unsplash.com/photo-1599460093135-f881f382fc26?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGJveGVyJTIwZG9nJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D',
-      role: 'Pet Owner',
+      role: 'Pet-Owner',
     },
     {
       name: 'John Smith',
       message: 'Very professional and caring staff. Highly recommend!',
       imageUrl:
         'https://plus.unsplash.com/premium_photo-1718652842683-8e7f623fbc1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fGJveGVyJTIwZG9nJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D',
-      role: 'Dog Trainer',
+      role: 'Dog-Trainer',
     },
     {
       name: 'Jane Doe',
