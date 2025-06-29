b81a5227edd76d6bdd5219f0191fde3163ac3038
Vladimir Gulev <vladinson009@icloud.com>
2025-06-29 23:11:23 +0200

    angular animations

---
+++ src/app/animations/carouselAnimation.ts
@@ -0,0 +1,26 @@
+import {
+  animate,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+export default function (ms: number) {
+  return trigger('carouselAnimation', [
+    transition(':increment', [
+      style({ transform: 'translateX(100%)', opacity: 0 }),
+      animate(
+        `${ms}ms ease-out`,
+        style({ transform: 'translateX(0)', opacity: 1 })
+      ),
+    ]),
+    transition(':decrement', [
+      style({ transform: 'translateX(-100%)', opacity: 0 }),
+      animate(
+        `${ms}ms ease-out`,
+        style({ transform: 'translateX(0)', opacity: 1 })
+      ),
+    ]),
+  ]);
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,14 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
     provideAnimations(),
   ],
 };
